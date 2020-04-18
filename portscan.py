2d57a1dfff3ddf7d8643cbdaf8c90f2851247500
Steven Jenkins <45439182+SJ-Hydra@users.noreply.github.com>
2020-04-18 13:18:01 -0400

    Update portscan.py

--- portscan.py
+++ portscan.py
@@ -1,2 +1,18 @@
 #!/usr/bin/python3
 
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.settimeout(6)
+
+host = input("Please enter the IP")     #IP
+
+port = int(input("Please enter the port")     #Port
+
+def portScanner(port):
+   if s.connect_ex((host, port)):
+      print("The port is closed")
+   else:
+      print("The port is open."
+
+portScanner(port)
